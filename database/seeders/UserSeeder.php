4f6b3fc06365d8a016f1eb04312c0d62fc0e1a40
Emerson <familiadaguiabatista@gmail.com>
2025-06-15 17:23:47 -0300

    login laravel

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -14,28 +15,28 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name'=> 'Emeson Meireles',
-                'username'=>'msflix',
-                'email'=>'emeson@gmail.com',
-                'role'=>'admin',
-                'status'=>'active',
-                'password'=>bcrypt('12345678')
+                'name' => 'Emeson Meireles',
+                'username' => 'msflix',
+                'email' => 'emeson@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => Hash::make('12345678'),
             ],
             [
-                'name'=> 'Vendedor',
-                'username'=>'vendor',
-                'email'=>'vendedor@gmail.com',
-                'role'=>'vendor',
-                'status'=>'active',
-                'password'=>bcrypt('12345678')
+                'name' => 'Vendedor',
+                'username' => 'vendor',
+                'email' => 'vendedor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => Hash::make('12345678'),
             ],
             [
-                'name'=> 'Cliente',
-                'username'=>'user',
-                'email'=>'cliente@gmail.com',
-                'role'=>'user',
-                'status'=>'active',
-                'password'=>bcrypt('12345678')
+                'name' => 'Cliente',
+                'username' => 'user',
+                'email' => 'cliente@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => Hash::make('12345678'),
             ],
         ]);
     }
